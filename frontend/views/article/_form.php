231459792cd469f807ca08a5e62e004bc9cc966d
mohit <monusidhar>
2023-07-25 00:00:22 +0530

    space remove

--- frontend/views/article/_form.php
+++ frontend/views/article/_form.php
@@ -12,7 +12,6 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-
     <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
 
     <?= $form->field($model, 'description')->textarea(['rows' => 6]) ?>
