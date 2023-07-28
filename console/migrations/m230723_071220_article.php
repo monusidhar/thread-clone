316fe7e5d2ff3e0915b8b35d1d50d16a8bee2843
mohit <monusidhar>
2023-07-29 00:13:54 +0530

    created and updated at

--- console/migrations/m230723_071220_article.php
+++ console/migrations/m230723_071220_article.php
@@ -16,6 +16,8 @@ class m230723_071220_article extends Migration
             'id' => $this->primaryKey(),
             'title' => $this->string()->notNull(),
             'description' => $this->text(),
+            'created_at' => $this->integer(),
+            'updated_at' => $this->integer(),
         ]);
 
     }
